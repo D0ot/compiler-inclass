cfe26f9ba409e2b89d0fb70ee7a788e99ec1583e
doot <gugegby@gmail.com>
2020-10-22 02:03:10 +0800

    lab3 60% finished

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,5 +1,7 @@
+from os import ttyname
 import sys
-from typing import Type
+from typing import ItemsView, Type
+from copy import copy
 
 
 class Token:
@@ -110,10 +112,10 @@ class Production:
     
     def __str__(self) -> str:
         outstr = ""
-        outstr += self.left.val + " -> "
+        outstr += self.left + " -> "
         for x in self.rights:
             for y in x:
-                outstr += y.val + " "
+                outstr += y + " "
             outstr = outstr + "| "
         return outstr[:-2]
 
@@ -121,30 +123,40 @@ class Production:
         return self.__str__()
 
 class Item:
-    def __init__(self, left_token, right_tokens, pos, next_token):
+    def __init__(self, left_token, right_tokens, pos, preview_token):
         self.left_token = left_token
         self.pos = pos
         self.right_tokens = right_tokens
-        self.next_token = next_token
+        self.preview_token = preview_token
     
+    def closureExtendable(self) -> bool:
+        return self.pos < len(self.right_tokens) and self.right_tokens[self.pos].isNonTerminal()
+
+    def closureGoable(self) -> bool:
+        return self.pos != len(self.right_tokens)
+
     def __str__(self) -> str:
         outstr = "[ {} -> ".format(self.left_token.val)
         i = 0
         once_flag = True
         while i < len(self.right_tokens):
             if i == self.pos and once_flag:
-                outstr += "*"
+                outstr += "• "
                 once_flag = False
             else:
                 outstr += self.right_tokens[i].val + " "
                 i = i + 1
         if once_flag:
-            outstr = outstr[:-1] + "*"
-        outstr += ", {} ]".format(self.next_token.val)
+            outstr = outstr[:-1] + " •"
+        outstr += ", {} ]".format(self.preview_token.val)
         return outstr
 
     def __repr__(self) -> str:
         return self.__str__()
+    
+    def __hash__(self) -> int:
+        return hash(self.__str__())
+
 class FirstSets:
 
     first_sets = {}
@@ -172,23 +184,21 @@ class FirstSets:
             changed_in_this_iter = False
 
             for prod in productions:
-                for right_strs in productions[prod].rights:
+                for candi_strs in productions[prod].rights:
 
                     left_token = ttab.getToken(productions[prod].left)
                     old_len = len(self.first_sets[left_token])
                     old_nullable = self.nullable[left_token]
                     
-                    rights = [ttab.getToken(x) for x in right_strs]
+                    candi_tokens = [ttab.getToken(x) for x in candi_strs]
 
                     i = 0
-                    while i < len(rights):
-                        if not self.nullable[rights[i]]:
-                            break
-                        self.first_sets[left_token] = self.first_sets[left_token].union(self.first_sets[rights[i]])
+                    while i < len(candi_tokens) and self.nullable[candi_tokens[i]]:
+                        self.first_sets[left_token] = self.first_sets[left_token].union(self.first_sets[candi_tokens[i]])
                         i = i + 1
 
-                    if i < len(rights):
-                        self.first_sets[left_token] = self.first_sets[left_token].union(self.first_sets[rights[i]])
+                    if i < len(candi_tokens):
+                        self.first_sets[left_token] = self.first_sets[left_token].union(self.first_sets[candi_tokens[i]])
                     else:
                         self.nullable[left_token] = True
                     
@@ -208,29 +218,53 @@ class FirstSets:
         else:
             raise ValueError("queryNullable not found")
     
+    def queryFirstOfSentence(self, tokens):
+        ret = set()
+        for x in tokens:
+            ret = ret.union(self.queryFirst(x))
+            if not self.queryNullable(x):
+                break
+        return ret
+    
     def __str__(self) -> str:
         return str(self.first_sets) + str(self.nullable)
 
 
 class LRAnalyzer:
+    ''' productions are special, indexed by string '''
     productions = {}
+
     ttab = TokenTable()
     items = {}
+    closures = {}
 
-    def __init__(self, pstr: list, eplision_str):
+
+    def __init__(self, pstr: list, eplision_str, argument_str, guard_str):
 
         self.eplision = Token(eplision_str, Token.TypeTerminal)
 
-        flag = True
-        for x in pstr:
+        orginal_first_prod = Production(pstr[0], self.ttab)
+        arguemnted_first_str = argument_str + " -> " + orginal_first_prod.left
+        argumented_pstr = [arguemnted_first_str] + pstr
+
+
+        for x in argumented_pstr:
             prod = Production(x, self.ttab)
             self.productions[prod.left] = prod
-            if flag:
-                self.start_token = prod.left
-                flag = False
         
+        
+        self.eplision_token = self.ttab.getToken(eplision_str)
+        self.argument_token = self.ttab.getToken(argument_str)
+        self.orginal_start_token = self.ttab.getToken(self.productions[self.argument_token.val].rights[0][0])
         self.first_sets = FirstSets(self.productions, self.ttab, self.eplision)
-        #self.generateItems()
+
+        self.guard_token = Token(guard_str, Token.TypeTerminal)
+        self.ttab.AddToken(self.guard_token)
+
+        self.first_sets.first_sets[self.guard_token] = {self.guard_token}
+        self.first_sets.nullable[self.guard_token] = False
+
+        self.generateClosureSet()
         
     
     def analyze(self, sentence : str):
@@ -243,23 +277,81 @@ class LRAnalyzer:
                 non_terminals.append(self.ttab.tokens[x])
         
         for x in self.ttab.tokens.keys():
-            token:Token = self.ttab.tokens[x]
+            token:Token = self.ttab.getToken(x)
             if token.type == Token.TypeNonTerminal:
-                prod:Production = self.productions[token]
+                prod:Production = self.productions[token.val]
 
                 tmp = []
-                for y in prod.rights:
-                    token_cnt = len(y)
+                for right_strs in prod.rights:
+                    str_cnt = len(right_strs)
                     i = 0
-                    while i <= token_cnt:
+                    while i <= str_cnt:
                         for c in non_terminals:
-                            item = Item(prod.left, y, i, c)
+                            right_tokens = [self.ttab.getToken(x) for x in right_strs]
+                            item = Item(self.ttab.getToken(prod.left), right_tokens, i, c)
                             tmp.append(item)
                         i = i + 1
-                self.items[prod.left] = tmp
+                self.items[self.ttab.getToken(prod.left)] = tmp
     
-    def generateClosureSet(self, item:Item):
-        pass
+    def generateClosureSet(self):
+        start_item = Item(self.argument_token, [self.orginal_start_token], 0, self.guard_token)
+        start_set = {start_item}
+
+        def closureHelper(iset:set):
+            item_stack = []
+            item_stack.extend(iset)
+            ret_set = set()
+            ret_set.update(iset)
+            while len(item_stack) != 0:
+                item:Item = item_stack.pop()
+                if item.closureExtendable():
+                    next_token = item.right_tokens[item.pos]
+                    prod:Production = self.productions[next_token.val]
+                    sentence = item.right_tokens[item.pos + 1:]
+                    sentence.append(item.preview_token)
+                    sts_fst = self.first_sets.queryFirstOfSentence(sentence)
+
+                    for candi_strs in prod.rights:
+                        candi_tokens = [self.ttab.getToken(x) for x in candi_strs]
+                        for preview_token in sts_fst:
+                            new_item = Item(next_token, candi_tokens, 0, preview_token)
+                            if new_item in ret_set:
+                                continue
+                            else:
+                                ret_set.update([new_item])
+                                item_stack.append(new_item)
+            return ret_set
+        
+        def closureGoHelper(iset:set):
+
+            ret_tmp = {}
+
+
+            def goHelper(item:Item) -> Item:
+                new_item = copy(item)
+                new_item.pos = new_item.pos + 1
+                return new_item
+
+
+            for item in iset:
+                item : Item
+                if item.closureGoable():
+                    new_item = goHelper(item)
+                    next_token = item.right_tokens[item.pos]
+                    if not next_token in ret_tmp.keys():
+                        ret_tmp[next_token] = []
+                    ret_tmp[next_token].append(new_item)
+
+            return [closureHelper(set(x)) for x in ret_tmp.values()]
+        
+
+                    
+        first_closure = closureHelper(start_set)
+        print(first_closure)
+        sets = closureGoHelper(first_closure)
+        for x in sets:
+            print(x)
+
 
         
     def __str__(self) -> str:
@@ -269,12 +361,21 @@ class LRAnalyzer:
         return self.__str()
 
 if __name__ == "__main__":
-    f = open("/home/doot/projects/compiler/lab3/testcase/case1.txt", "r")
+    f = open("/home/doot/projects/compiler/lab3/testcase/case2.txt", "r")
     plist = f.readlines()
-    lra = LRAnalyzer(plist, "?")
+    lra = LRAnalyzer(plist, "?", "START", "END")
 
     for x in lra.first_sets.first_sets:
         print(x, lra.first_sets.first_sets[x])
     
     for x in lra.first_sets.nullable:
         print(x, lra.first_sets.nullable[x])
+    
+    # print items
+    '''for x in lra.items:
+        for y in lra.items[x]:
+            print(y)'''
+    
+    for x in lra.productions:
+        print(lra.productions[x])
+    
